fbcd35d01eef4c4b9e04fec117a01f7bbad6180b
kdm2730 <kdm2730@rit.edu>
2015-10-29 11:42:09 -0400

    Moved direct calls from the views to model controllers

--- app/controllers/project_controller.rb
+++ app/controllers/project_controller.rb
@@ -1,6 +1,7 @@
 class ProjectController < ApplicationController
   def show
     @project = Project.find(params[:id])
+    @array = Event.where(name: @project.name).order(:date)
   end
   def index
   end
