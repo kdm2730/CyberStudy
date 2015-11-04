d8a37d5e7b9b5b41571842adcc0083c760e2e13b
kdm2730 <kdm2730@rit.edu>
2015-11-04 13:11:56 -0500

    Added associations between models

--- db/migrate/20151026032353_create_contributors.rb
+++ db/migrate/20151026032353_create_contributors.rb
@@ -3,6 +3,8 @@ class CreateContributors < ActiveRecord::Migration
     create_table :contributors do |t|
       t.string :author
       t.string :cve
+      t.string :company
+      t.string :project
       t.timestamps null: false
     end
   end
