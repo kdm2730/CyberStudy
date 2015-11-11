576f81660c07e3a304ac9e799976ab237328d39c
kdm2730 <kdm2730@rit.edu>
2015-11-11 13:27:27 -0500

    Attempted github_api gem implementation

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,9 @@
 class WelcomeController < ApplicationController
   def index
+  	@arr = []
+  	repos = Github::Client::Repos.new
+	repos.list user: 'andymeneely', repo: 'ffmpeg-history' do |list|
+  		@arr.push(list.name)
+	end
   end
 end
