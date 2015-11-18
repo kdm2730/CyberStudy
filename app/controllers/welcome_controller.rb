feb26bb7451f15dd9521a488e9ce517f01db4a5c
kdm2730 <kdm2730@rit.edu>
2015-11-18 01:20:03 -0500

    Created seed data to be pulled off ffmpeg-history

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,9 +1,8 @@
 class WelcomeController < ApplicationController
   def index
-  	@arr = []
-  	repos = Github::Client::Repos.new
-	repos.list user: 'andymeneely', repo: 'ffmpeg-history' do |list|
-  		@arr.push(list.name)
-	end
+  	github = Github.new user: 'andymeneely', repo: 'ffmpeg-history'
+  	cves_yml = Base64.decode64(github.repos.contents.get(path: 'cves.yml').content)
+  	@cves = YAML.load(cves_yml)
+  	@cvekeys = @cves.keys
   end
 end
